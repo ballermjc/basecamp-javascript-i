a4e5420cb0ca448311f261fb6804696e0dbecb02
Michael J Cooper <ballermjc@gmail.com>
2017-11-03 05:16:12 -0600

    first 6 problems

--- assessment.js
+++ assessment.js
@@ -1,45 +1,47 @@
 // #1 create a variable called 'myName' and assign it to your name as a string.
 
 // CODE HERE
-
+var myName = "Michael";
 // #2 create a variable called 'iLoveCode' and assign it the value of 'myName' + loves code as a string.
 
 // CODE HERE
-
+var iLoveCode = myName + " loves code";
 // ARROW FUNCTIONS
 // #3 using the arrow syntax, create a function called 'wildNameReturn' that takes in variable as a parameter. Return the variable inside of the function with the word 'wild' in front so it returns "wild Bryan" and then invoke wildNameReturn using the 'myName' variable you created as an argument.
 
 // CODE HERE
-
+var wildNameReturn = word => "wild " + word;
+wildNameReturn(myName);
 // #4 using the arrow syntax, create a function called 'sum' that takes in a number and returns that number added to itself.
 
 // CODE HERE
-
+var sum = num => num + num;
 // #5 using the arrow syntax, create a function called 'greeter' that takes in two parameters, name & age. greeter should return a string like this "Hi, I'm Bryan and I am 27 years old." but replacing the name and age with the passed in values. The test on this question is very strict on casing and punctuation.
 
 // CODE HERE
-
+var greeter = (name, age) => "Hi, I'm " + name + " and I am " + age + " years old.";
 // #6 using the arrow syntax, create a function called 'animalSounds'. animalSound takes three parameters: name, sound and a callback. animalSound should return something like this "I am wild Bryan. Hear me woof." when invoked using the wildNameReturn function you previously created as the callback.
 // CODE HERE
-
+var animalSounds = (name, sound, callback) => "I am " + wildNameReturn(name) + ". Hear me " + sound + ".";
 // CALLBACKS
 // Here we will write a function that takes in a callback as a parameter. You must write the callback function to make the existing function work properly.
 // EXAMPLE
 // You would create the sayHi function here
-var sayHi = (str, callback) =>{
-  callback(str);
- }
-// So you can use it down here
- sayHi('Hi Lindsey', thingToSay =>{
-    alert(thingToSay); //should alert ('Hi Lindsey')'
- });
+// var sayHi = (str, callback) =>{
+//   callback(str);
+//  }
+// // So you can use it down here
+//  sayHi('Hi Lindsey', thingToSay =>{
+//     alert(thingToSay); //should alert ('Hi Lindsey')'
+//  });
  // You can comment out the above code so it doesn't run every time you refresh your page.
 
 // #7 using the arrow syntax, write a function called 'first' that returns the first item of the array
 var names = ['Bryan', 'Jeremy', 'Joe', 'Megan', 'Ian', 'Taylor'];
 
 // CODE HERE
-
+var first = (arr) => arr[0];
+var firstname = first(names);
 first(names, firstName => {
   console.log(`The first name in names is ${firstName}`);
   return firstName;
@@ -48,7 +50,7 @@ first(names, firstName => {
 // #8 using the arrow syntax, write a function called 'multiply' that multiplies two numbers. Invoke the callback with the result of the multiplication.
 
 // CODE HERE
-
+var multiply = (x, y) => x * y;
 multiply(4,3, answer => {
   console.log(`the answer is ${answer}`)
 });
@@ -56,7 +58,7 @@ multiply(4,3, answer => {
 // #9 using the arrow syntax, write a function called 'last' that returns the last name in the previously crated names array using a callback function.
 
 // CODE HERE
-
+var last = arr => arr[arr.length - 1];
 last(names, lastName =>{
   console.log(`The last name in names is ${lastName}`);
   return lastName;
@@ -65,7 +67,15 @@ last(names, lastName =>{
 // #10 using the arrow syntax, write a function called 'sizer' that returns the larger of two numbers that are passed in using a callback function.
 
 // CODE HERE
-
+var sizer = (x, y) => {
+  if (x > y) {
+    return x;
+  } else if (y > x) {
+    return y;
+  } else {
+    return "Equal";
+  }
+};
 sizer(300, 33, bigger => {
   console.log(`${bigger} is the larger number`);
   return bigger;
